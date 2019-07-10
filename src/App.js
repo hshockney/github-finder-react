747e1f8d6f7a1224397f02dd5cd10e9feec7cfd8
hshockney <shockney@atlanticbb.net>
2019-07-10 13:03:43 -0400

    Pulling users from github api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = { users: [], loading: false };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await axios.get('https://api.github.com/users');
+    this.setState({ users: response.data, loading: false });
+  }
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github icon' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
